0854708d95a112c4677d98f4b19af80f2f627dfd
Sheetal <sheetal.raghav0818@gmail.com>
2022-12-17 16:46:09 +0530

    file added

---
+++ index.js
@@ -0,0 +1,43 @@
+// if (process.env.NODE_ENV !== 'production') {
+    
+// }
+const express = require('express')
+const app = express()
+// const axios=require('axios')
+// const cheerio=require('cheerio')
+require('dotenv').config();
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT ;
+const dbUrl = process.env.DB_URL ;
+const mongoose = require('mongoose');
+mongoose.connect(dbUrl, () => console.log("Database Connected"))
+const postRoutes = require('./routes/post')
+const cors = require('cors');
+ const corsOptions = {
+     origin: 'https://insta-frontend-gasx.onrender.com',
+     credentials: true,            //access-control-allow-credentials:true
+     optionSuccessStatus: 200
+ }
+app.use(cors(corsOptions));
+
+
+app.use(express.urlencoded());
+app.use(bodyParser.json());
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('form.ejs');
+})
+
+
+
+app.use('/post', postRoutes);
+
+app.use('/*', (req, res) => {
+    res.status(404).send('Page Not Found');
+})
+
+
+app.listen(PORT, () => console.log('Server is on 3000', PORT));
