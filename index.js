e12f7abaf403355974a5e17eadd57d4099ee2218
Sheetal Raghav <119871180+Sheetal-Raghav@users.noreply.github.com>
2022-12-17 19:03:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const mongoose = require('mongoose');
 mongoose.connect(dbUrl, () => console.log("Database Connected"))
 const postRoutes = require('./routes/post')
 const cors = require('cors');
- const corsOptions = {
-      origin: 'https://insfrontend.onrender.com',
-     credentials: true,            //access-control-allow-credentials:true
-     optionSuccessStatus: 200
-  }
-app.use(cors(corsOptions));
+//  const corsOptions = {
+//       origin: 'https://insfrontend.onrender.com',
+//      credentials: true,            //access-control-allow-credentials:true
+//      optionSuccessStatus: 200
+//   }
+app.use(cors());
 
 
 app.use(express.urlencoded());
